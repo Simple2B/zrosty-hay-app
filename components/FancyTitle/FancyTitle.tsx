c055aa8322bce723684fda68a9ee535e90be783a
Taraslysak <lysak.ipr@gmail.com>
2024-01-22 17:45:07 +0200

    Add VS Code settings, exclude files from tsconfig, update Storybook configuration, and fix linting and type-checking issues

--- components/FancyTitle/FancyTitle.tsx
+++ components/FancyTitle/FancyTitle.tsx
@@ -1,31 +1,27 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import React from 'react';
+import { Button, Text, View } from 'react-native';
 
 type FancyTitleProps = {
-  text: string;
-  size: "small" | "medium" | "large";
-  onPress: (title: string) => void;
+	text: string;
+	size: 'small' | 'medium' | 'large';
+	onPress: (title: string) => void;
 };
 
 const SIZES = {
-  small: 16,
-  medium: 24,
-  large: 32,
+	small: 16,
+	medium: 24,
+	large: 32,
 };
 
-export const FancyTitle = ({
-  text,
-  size = "medium",
-  onPress,
-}: FancyTitleProps) => {
-  const handlePress = () => {
-    onPress(text);
-  };
+export const FancyTitle = ({ text, size = 'medium', onPress }: FancyTitleProps) => {
+	const handlePress = () => {
+		onPress(text);
+	};
 
-  return (
-    <View style={{ backgroundColor: "gold" }}>
-      <Text style={{ fontSize: SIZES[size] }}>{text}</Text>
-      <Button title="Click me" onPress={handlePress} />
-    </View>
-  );
+	return (
+		<View style={{ backgroundColor: 'gold' }}>
+			<Text style={{ fontSize: SIZES[size] }}>{text}</Text>
+			<Button title='Click me' onPress={handlePress} />
+		</View>
+	);
 };
--- orval.config.ts
+++ orval.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "orval";
+import { defineConfig } from 'orval';
 
 module.exports = defineConfig({
-  base: {
-    input: "./openapi.json",
-    output: {
-      baseUrl: "https://api.zrosty-hay.simple2b.net",
-      target: "./src/api/zrosty_hay.ts",
-      client: "react-query",
-      mode: "tags-split",
-      schemas: "src/api/model",
-      mock: true,
-    },
-  },
+	base: {
+		input: './openapi.json',
+		output: {
+			baseUrl: 'https://api.zrosty-hay.simple2b.net',
+			target: './src/api/zrosty_hay.ts',
+			client: 'react-query',
+			mode: 'tags-split',
+			schemas: 'src/api/model',
+			mock: true,
+		},
+	},
 });
