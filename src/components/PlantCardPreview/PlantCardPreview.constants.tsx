8a73252a41a61bb132b210fde2b8d4208890ca3e
marynag <mgrabchak1109@gmail.com>
2024-01-30 14:43:15 +0200

    fix image and svg

--- src/components/PlantCardPreview/PlantCardPreview.constants.tsx
+++ src/components/PlantCardPreview/PlantCardPreview.constants.tsx
@@ -4,7 +4,7 @@ import Ruler from '@assets/icons/plantCard/ruler.svg';
 import Plant from '@assets/icons/plantCard/plant.svg';
 import Thermometer from '@assets/icons/plantCard/thermometer.svg';
 
-export enum Category {
+export enum CardDescriptionIcons {
 	plant = 'plant',
 	rainDrop = 'rainDrop',
 	ruler = 'ruler',
@@ -12,10 +12,10 @@ export enum Category {
 	thermometer = 'thermometer',
 }
 
-export const categoryToSvg = {
-	[Category.sun]: Sun,
-	[Category.rainDrop]: RainDrop,
-	[Category.ruler]: Ruler,
-	[Category.plant]: Plant,
-	[Category.thermometer]: Thermometer,
+export const cartDescriptionToSvg = {
+	[CardDescriptionIcons.sun]: Sun,
+	[CardDescriptionIcons.rainDrop]: RainDrop,
+	[CardDescriptionIcons.ruler]: Ruler,
+	[CardDescriptionIcons.plant]: Plant,
+	[CardDescriptionIcons.thermometer]: Thermometer,
 };
--- src/components/PlantCardPreview/PlantCardPreview.tsx
+++ src/components/PlantCardPreview/PlantCardPreview.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useStyles } from 'react-native-unistyles';
 import { styleSheet } from './PlantCardPreview.style';
-import { View } from '../Themed';
 import { PlantCardInfo } from '../PlantCardInfo/PlantCardInfo';
 import { Plant } from '@src/api/model/plant';
-import { Category, categoryToSvg } from './PlantCardPreview.constants';
+import { CardDescriptionIcons, cartDescriptionToSvg } from './PlantCardPreview.constants';
+import FastImage from 'react-native-fast-image';
 
 type PlantCardPreviewProps = {
 	plantInfo: Plant;
@@ -19,32 +19,33 @@ export const PlantCardPreview = ({ plantInfo }: PlantCardPreviewProps) => {
 
 	const { t } = useTranslation();
 
-	const isSunLovingText = isSunLoving
-		? t('components.plantCardPreview.isSunLoving')
-		: t('components.plantCardPreview.notSunLoving');
+	const isSunLovingText = isSunLoving ? t('components.isSunLoving') : t('components.notSunLoving');
 	const temperatureText = `${minTemperature} - ${maxTemperature} °C`;
-	const careTypeText = t(`components.plantCardPreview.care.${careType}`);
+	const careTypeText = t(`components.care.${careType}`);
 	const sizeText = `${minSize} - ${maxSize} cm`;
-	const wateringText = t(`components.plantCardPreview.watering.${watering}`);
+	const wateringText = t(`components.watering.${watering}`);
 
 	//TODO: add photo template in  case no picture
 	return (
-		<View style={styles.container}>
-			<Image
+		<TouchableOpacity style={styles.cardWrapper}>
+			<FastImage
 				style={styles.plantImage}
-				resizeMode='contain'
 				source={{
 					uri: photo?.urlPath || '',
+
+					priority: FastImage.priority.normal,
 				}}
+				resizeMode={FastImage.resizeMode.cover}
 			/>
+
 			<View style={styles.infoBlock}>
 				<Text style={styles.plantName}>{name}</Text>
-				<PlantCardInfo title={isSunLovingText} SvgIcon={categoryToSvg[Category.sun]} />
-				<PlantCardInfo title={wateringText} SvgIcon={categoryToSvg[Category.rainDrop]} />
-				<PlantCardInfo title={temperatureText} SvgIcon={categoryToSvg[Category.thermometer]} />
-				<PlantCardInfo title={careTypeText} SvgIcon={categoryToSvg[Category.plant]} />
-				<PlantCardInfo title={sizeText} SvgIcon={categoryToSvg[Category.ruler]} />
+				<PlantCardInfo title={isSunLovingText} SvgIcon={cartDescriptionToSvg[CardDescriptionIcons.sun]} />
+				<PlantCardInfo title={wateringText} SvgIcon={cartDescriptionToSvg[CardDescriptionIcons.rainDrop]} />
+				<PlantCardInfo title={temperatureText} SvgIcon={cartDescriptionToSvg[CardDescriptionIcons.thermometer]} />
+				<PlantCardInfo title={careTypeText} SvgIcon={cartDescriptionToSvg[CardDescriptionIcons.plant]} />
+				<PlantCardInfo title={sizeText} SvgIcon={cartDescriptionToSvg[CardDescriptionIcons.ruler]} />
 			</View>
-		</View>
+		</TouchableOpacity>
 	);
 };
