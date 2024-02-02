8e064e086704668b2f222131758de7c5d72948fa
nazarkobryn <kobrinnazar05@gmail.com>
2024-02-02 14:06:40 +0200

    fix padiings

--- src/screens/Plants/Plant.style.tsx
+++ src/screens/Plants/Plant.style.tsx
@@ -4,8 +4,6 @@ export const styleSheet = createStyleSheet((theme) => ({
 	wrapper: {
 		flex: 1,
 		gap: theme.size.md,
-		paddingTop: theme.size.sm,
-		paddingBottom: theme.size.sm,
 	},
 	plants: {
 		paddingHorizontal: theme.size.lg,
