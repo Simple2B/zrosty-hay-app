c416eba691228b4305606ea90ac0ef5f523754b2
nazarkobryn <kobrinnazar05@gmail.com>
2024-03-01 17:49:25 +0200

    refactoring biome

--- src/screens/Plants/Plants.tsx
+++ src/screens/Plants/Plants.tsx
@@ -1,37 +1,26 @@
-import { useEffect, useState } from "react";
-import { useStyles } from "react-native-unistyles";
-import { View } from "react-native";
-import { FlashList } from "@shopify/flash-list";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useEffect, useState } from 'react';
+import { useStyles } from 'react-native-unistyles';
+import { View } from 'react-native';
+import { FlashList } from '@shopify/flash-list';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { Categories } from "@src/components/Categories/Categories";
-import { PlantsHeader } from "@src/components/PlantsHeader/PlantsHeader";
-import { useAPIGetAllInfinite } from "@src/api/plants/plants";
-import { queryKeys } from "@src/constants/queryKeys";
-import { Spinner } from "@src/components/Spinner/Spinner";
-import { styleSheet } from "./Plant.style";
-import {
-	renderItemPlantCardPreview,
-	getKeyExtractor,
-	getNextPlantPage,
-} from "./Plant.callbacks";
+import { Categories } from '@src/components/Categories/Categories';
+import { PlantsHeader } from '@src/components/PlantsHeader/PlantsHeader';
+import { useAPIGetAllInfinite } from '@src/api/plants/plants';
+import { queryKeys } from '@src/constants/queryKeys';
+import { Spinner } from '@src/components/Spinner/Spinner';
+import { styleSheet } from './Plant.style';
+import { renderItemPlantCardPreview, getKeyExtractor, getNextPlantPage } from './Plant.callbacks';
 
 const ITEM_SIZE = 182;
 const PAGINATION_SIZE = 20;
 
 export default function PlantsScreen() {
 	const { styles } = useStyles(styleSheet);
-	const [searchInput, setSearchInput] = useState<string>("");
+	const [searchInput, setSearchInput] = useState<string>('');
 	const [categoryUuids, setCategoryUuids] = useState<string[]>([]);
 
-	const {
-		data,
-		isLoading,
-		isFetchingNextPage,
-		fetchNextPage,
-		hasNextPage,
-		refetch
-	} = useAPIGetAllInfinite(
+	const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage, refetch } = useAPIGetAllInfinite(
 		{
 			size: PAGINATION_SIZE,
 			name: searchInput,
@@ -39,7 +28,7 @@ export default function PlantsScreen() {
 		},
 		{
 			query: {
-				queryKey: [queryKeys.GET_PLANTS,  categoryUuids],
+				queryKey: [queryKeys.GET_PLANTS, categoryUuids],
 				getNextPageParam: getNextPlantPage,
 			},
 			axios: {
@@ -47,21 +36,19 @@ export default function PlantsScreen() {
 					indexes: null,
 				},
 			},
-		}
+		},
 	);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
-			refetch()
+			refetch();
 		}, 1500);
 		return () => clearTimeout(timer);
 	}, [searchInput]);
 
 	const handleSelectCategory = (uuid: string) => {
 		if (categoryUuids.includes(uuid)) {
-			setCategoryUuids([
-				...categoryUuids.filter((curUuid) => curUuid !== uuid),
-			]);
+			setCategoryUuids([...categoryUuids.filter((curUuid) => curUuid !== uuid)]);
 		} else {
 			setCategoryUuids([...categoryUuids, uuid]);
 		}
@@ -83,12 +70,9 @@ export default function PlantsScreen() {
 	};
 
 	return (
-		<SafeAreaView style={styles.wrapper} edges={["bottom"]}>
+		<SafeAreaView style={styles.wrapper} edges={['bottom']}>
 			<PlantsHeader value={searchInput} onChangeText={setSearchInput} />
-			<Categories
-				categoryUuids={categoryUuids}
-				handleSelectCategory={handleSelectCategory}
-			/>
+			<Categories categoryUuids={categoryUuids} handleSelectCategory={handleSelectCategory} />
 			{isLoading && <Spinner size={64} />}
 			{!!plants && (
 				<FlashList
