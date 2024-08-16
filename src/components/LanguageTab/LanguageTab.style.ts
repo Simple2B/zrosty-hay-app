8787b5e7b54b1fcd06a0384b376cbe6ae613a814
marynag <mgrabchak1109@gmail.com>
2024-08-16 12:21:50 +0300

    fix: dynamic style change by unistlyes

--- src/components/LanguageTab/LanguageTab.style.ts
+++ src/components/LanguageTab/LanguageTab.style.ts
@@ -1,13 +1,18 @@
 import { createStyleSheet } from 'react-native-unistyles';
 
 export const styleSheet = createStyleSheet((theme) => ({
-	tabWrapper: (isSelected: boolean) => ({
+	tabWrapper: {
 		textAlign: 'center',
 		borderWidth: 1,
-		borderColor: isSelected ? theme.colors.primary : theme.colors.borderGrey,
 		borderRadius: theme.size.lg,
 		padding: theme.size.md + theme.size.xxs,
 		width: '100%',
 		flex: 1,
-	}),
+	},
+	selected: {
+		borderColor: theme.colors.primary,
+	},
+	unselected: {
+		borderColor: theme.colors.borderGrey,
+	},
 }));
--- src/components/LanguageTab/LanguageTab.tsx
+++ src/components/LanguageTab/LanguageTab.tsx
@@ -5,21 +5,21 @@ import { styleSheet } from './LanguageTab.style';
 import { useTranslation } from 'react-i18next';
 import { Language } from '@src/constants/languages';
 
-type Props = {
-	language: string;
+type LanguageTabProps = {
+	language: Language;
 	isSelected: boolean;
 	onSelect: (lang: Language) => void;
 };
 
-export const LanguageTab = ({ language, isSelected, onSelect }: Props) => {
+export const LanguageTab = ({ language, isSelected, onSelect }: LanguageTabProps) => {
 	const { t } = useTranslation();
 	const { styles } = useStyles(styleSheet);
 
 	const handleSelected = () => {
-		onSelect(language as Language);
+		onSelect(language);
 	};
 	return (
-		<Text style={styles.tabWrapper(isSelected)} onPress={handleSelected}>
+		<Text style={[styles.tabWrapper, isSelected ? styles.selected : styles.unselected]} onPress={handleSelected}>
 			{t(language)}
 		</Text>
 	);
