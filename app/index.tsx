6aa39c20966dd6326941c9b9bfe899274e32d1b7
marynag <mgrabchak1109@gmail.com>
2024-08-23 17:35:15 +0300

    add: Onboarding component with conditional rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
 import { useOnboardingStatus } from '@src/hooks/useOnboardingStatus';
 import { OnboardingScreen } from '@src/components/Onboarding/Onboarding';
+import { useMe } from '@src/hooks/useMe';
 import LoginScreen from '@src/screens/Login/Login';
+import PlantsScreen from '@src/screens/Plants/Plants';
 
 export default function Onboarding() {
 	const { hasOnboardingSeen } = useOnboardingStatus();
+	const user = useMe();
 
-	return <>{hasOnboardingSeen ? <LoginScreen /> : <OnboardingScreen />}</>;
+	if (!hasOnboardingSeen) {
+		return <OnboardingScreen />;
+	}
+
+	if (user) {
+		return <PlantsScreen />;
+	}
+
+	return <LoginScreen />;
 }
