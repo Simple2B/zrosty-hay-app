0bc4b288a5e5cd764843c72696572ffa93f06b75
marynag <mgrabchak1109@gmail.com>
2024-08-28 14:16:07 +0300

    fix: recipe padding top by fix SafeAreaView import

--- src/screens/RecipeDetailScreen/RecipeDetailScreen.tsx
+++ src/screens/RecipeDetailScreen/RecipeDetailScreen.tsx
@@ -1,16 +1,16 @@
 import { useStyles } from 'react-native-unistyles';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { styleSheet } from './RecipeDetailScreen.style';
 import { TRecipeScreenProps } from '@src/types/recipe';
 import { RecipePhotoSwiper } from '@src/components/RecipePhotoSwiper/RecipePhotoSwiper';
 import { RecipeDetail } from '@src/components/RecipeDetail/RecipeDetail';
 
-export function RecipeDetailScreen({ recipeUuid, }: TRecipeScreenProps) {
+export function RecipeDetailScreen({ recipeUuid }: TRecipeScreenProps) {
 	const { styles } = useStyles(styleSheet);
 
 	return (
-		<SafeAreaView style={styles.wrapper}>
+		<SafeAreaView style={styles.wrapper} edges={['bottom']}>
 			<RecipePhotoSwiper recipeUuid={recipeUuid} />
 			<RecipeDetail recipeUuid={recipeUuid} />
 		</SafeAreaView>
