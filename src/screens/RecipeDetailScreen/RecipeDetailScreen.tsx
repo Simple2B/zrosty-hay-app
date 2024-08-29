b82866e2b78598b37a06d582d203fcc8fdb40143
marynag <mgrabchak1109@gmail.com>
2024-08-29 10:58:23 +0300

    fix: replace  save area edges ['bottom'] to constants

--- src/screens/RecipeDetailScreen/RecipeDetailScreen.tsx
+++ src/screens/RecipeDetailScreen/RecipeDetailScreen.tsx
@@ -5,12 +5,13 @@ import { styleSheet } from './RecipeDetailScreen.style';
 import { TRecipeScreenProps } from '@src/types/recipe';
 import { RecipePhotoSwiper } from '@src/components/RecipePhotoSwiper/RecipePhotoSwiper';
 import { RecipeDetail } from '@src/components/RecipeDetail/RecipeDetail';
+import { SAFE_AREA_EDGES } from '@src/constants/layout';
 
 export function RecipeDetailScreen({ recipeUuid }: TRecipeScreenProps) {
 	const { styles } = useStyles(styleSheet);
 
 	return (
-		<SafeAreaView style={styles.wrapper} edges={['bottom']}>
+		<SafeAreaView style={styles.wrapper} edges={SAFE_AREA_EDGES}>
 			<RecipePhotoSwiper recipeUuid={recipeUuid} />
 			<RecipeDetail recipeUuid={recipeUuid} />
 		</SafeAreaView>
